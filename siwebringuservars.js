41d2e1ef0c51568a13e06d7e15df3e0100d9134f
xCrystiiAngel <intern3txang3l@gmail.com>
2026-07-05 13:31:52 +0700

    fixed stuff

---
+++ siwebringuservars.js
@@ -0,0 +1,5 @@
+var name = "Crystal";
+var thing = "iceboat racing";
+var character = "LeonSBU";
+var media = "Skyblock Kingdoms S2";
+var image = "https://xcrystiiangel.neocities.org/selfinsert.png"
