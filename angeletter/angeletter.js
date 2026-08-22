6445ec513467f371473762c28e621a6272dec879
xCrystiiAngel <intern3txang3l@gmail.com>
2026-08-22 20:30:49 +0700

    improved css

---
+++ angeletter/angeletter.js
@@ -0,0 +1,56 @@
+/*
+⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⣀⣀⠆⡡⢀⠔⠉⢀⠠⠀⠄⠠⠠⠶⠶⠤⣁⡦⢚⡟⠀⠙⡿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⡀⣠⡤⠦⢑⢀⢫⠁⢩⠈⠀⡄⢈⠠⠀⠌⢀⠂⠄⠠⠘⡷⣍⠫⡀⢁⢀⣠⢸⠇⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢐⡾⠍⠀⠀⠀⠁⢏⠢⡕⣦⡘⡇⢀⠆⠐⡈⠠⠀⠌⠐⠀⠐⠠⣸⣶⣶⣺⣿⡜⡘⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣝⣳⠀⠀⠀⠀⠀⠈⢆⣏⣧⢹⠀⠄⡈⠐⣇⠄⡀⠀⠀⢀⡐⢣⡆⡝⢿⠡⢍⡹⡱⠀⠀⠀⠠⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢻⣭⠀⠀⠀⠀⠀⠀⢸⣋⠟⡸⠀⠄⢁⢂⣿⡀⡡⠔⡆⢎⡱⠚⡕⢑⢺⡶⣬⣡⣷⠆⠀⠀⠂⠤⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢊⢾⠀⠀⠀⠀⡀⡀⠸⡍⣜⢘⠠⢈⡀⢾⠆⠘⣌⡹⣜⢢⣱⡩⣷⣴⠻⣟⡿⣺⡽⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠨⣃⠀⠀⠀⠀⢁⢨⠔⢏⣷⢀⠩⡀⡏⠀⠀⠀⠒⠉⠉⠀⠀⣿⣿⠀⡇⠠⡇⠔⢫⡀⡀⠀⠀⠀⠀⠀⠀⡠⢠
+⠀⠀⠀⠀⠀⠀⠑⠄⠀⠰⠕⠁⠀⣦⠀⠆⡂⢍⣀⣠⠤⠍⠀⠀⠀⠀⠀⠀⠃⢀⡇⢡⠇⠀⠀⠋⢒⠴⣀⡀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⢀⡜⠣⣤⡀⠀⠀⠐⠄⣡⠘⠤⢌⠀⠀⠀⠀⠀⠀⠀⢤⡀⠀⢸⡃⠦⠀⠀⠀⠀⠀⠀⠁⠂⢒⣄⠀⠀⠀
+⠀⠀⠀⠀⢀⠰⠁⠀⠀⠈⠄⡠⡀⠀⠀⠀⢼⢠⢋⡎⢶⣲⡔⢤⣤⣺⢸⠊⡦⠈⣇⢹⡀⠀⠈⠀⠀⠀⠀⠀⠀⠁⣂⠀⠀
+⠀⠀⢀⡔⠜⠀⠀⠀⠀⠀⠈⠔⡐⡀⠀⠀⠐⡲⣥⢛⠳⢽⠶⣄⠙⠻⠀⣂⣹⣷⠣⢏⡖⡄⠀⠀⠀⠀⠀⠀⠀⠀⠡⠀⠀
+⠀⢠⢃⠎⠀⠀⠀⠀⠀⠀⠀⠀⠔⠰⠀⢀⠈⠁⠁⠁⠚⢤⣚⠓⣋⠀⠀⠈⡎⡅⣇⠀⠉⠸⡀⠄⠀⠀⠀⠀⠀⢀⠳⠀⠀
+⢀⡌⢾⠀⡀⠀⠀⠀⠀⠀⠀⡠⢀⠴⠀⠈⡀⠄⠀⡀⠀⠬⠭⢂⡩⠀⠀⠀⢡⢑⡛⢐⡄⠀⠁⠂⠄⠀⠀⠀⢀⣘⠂⠀⠀
+⢄⡻⠄⠐⠄⠀⠀⠀⠀⠀⠠⣀⠃⠁⠀⠃⠈⠀⠀⢀⡘⠉⠉⣋⢕⡒⣀⠔⠁⡌⡆⠠⠀⡁⠀⠀⠈⠈⣢⠐⠆⠁⠀⠀⠀
+⢂⠳⡇⠀⠀⠀⠀⠀⡀⠠⠀⠁⠀⠀⠐⠀⠀⠀⢀⢃⠪⣑⡄⠢⢝⡖⢁⠒⠨⠌⠃⠠⠠⠇⠀⠀⠀⠲⠤⣋⡀⠀⠀⠀⠀
+⡌⡜⡅⠀⠀⠀⣠⡚⠁⠀⠀⠀⠀⠀⡄⠀⡀⢀⠀⠀⢃⠆⣸⡀⠀⠇⠀⠈⠱⠈⡔⡒⡀⢑⢀⢪⠔⡙⠀⠠⠓⠀⠀⠀⠀
+i have no fucking idea what im doing
+*/
+const GOOGLE_FORM_ID = "1FAIpQLSeOkg2F_NyPjeJBdRqEF_tLsJmCnvH6JrcYS8LPYXij9uwGNA";
+const ENTRY_ID = "entry.2144241403";
+const GOOGLE_SHEET_ID = "1tTAq2B57nwI5WlWV6nStlrYGHQ0j0FNsExzKWf1JtyI";
+
+const CLIENT_ID = "b4fb95e0edc434c";
+const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/" + GOOGLE_SHEET_ID + "/export?format=csv";
+const GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/" + GOOGLE_FORM_ID + "/formResponse";
+
+
+document.getElementById("submit").addEventListener("click", async function () {
+  const submitButton = document.getElementById("submit");
+  const statusText = document.getElementById("status");
+
+  submitButton.disabled = true;
+  statusText.textContent = "Uploading...";
+
+try {
+const googleFormData = new FormData();
+    googleFormData.append(ENTRY_ID, angeLetter);
+
+    await fetch(GOOGLE_FORM_URL, {
+      method: "POST",
+      body: googleFormData,
+      mode: "no-cors",
+    });
+
+    statusText.textContent = "Upload successful!";
+    alert("Letter sent!!! Thank you for your submission ^^");
+    location.reload();
+  } catch (error) {
+    console.error(error);
+    statusText.textContent = "Error sending message :(";
+    alert("Wuh oh ... the mail got lost to the web sea ... try again later !");
+  } finally {
+    submitButton.disabled = false;
+  }
+});
